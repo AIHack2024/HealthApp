30d81672debe1d51ac8b61c0c49b4d04f2535afb
idan gomberg <idangomberg@gmail.com>
2024-02-24 03:04:17 -0500

    init

--- HealthApp/Data/ApplicationDbContext.cs
+++ HealthApp/Data/ApplicationDbContext.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HealthApp.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace HealthApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<AutoimmuneDiseaseEntry> Patients { get; set; }
+       
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+           
         }
     }
 }
---
+++ HealthApp/Models/AutoimmuneDiseaseEntry.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthApp.Models
+{
+    public class AutoimmuneDiseaseEntry
+    {
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Gender { get; set; }
+
+        [Required]
+        public string Symptoms { get; set; }
+
+        [Required]
+        public string LaboratoryTests { get; set; }
+
+        public string FamilyHistory { get; set; }
+
+        public string PreviousDiagnosis { get; set; }
+
+        public string? AutoimmuneDisease { get; set; }
+    }
+}
